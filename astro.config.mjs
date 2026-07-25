cac6c444bee56fe16952aceb641b542cc2cd6383
James Ashley <james33ashley@gmail.com>
2026-07-25 17:22:16 -0500

    perf improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://jamesashley.dev',
   integrations: [mdx()],
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Inter',
+        cssVariable: '--font-sans',
+        styles: ['normal'],
+        weights: [300, 400, 500],
+      },
+    ],
+  },
 });
